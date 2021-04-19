29fcb8e4b2940a28df09d06e7b1f142568dd6aee
akashv001 <singh808798akash@gmail.com>
2021-04-19 23:40:20 +0530

    java practice code

---
+++ src/codingNinja/ArrayQues/sumof_two_Arrays.java
@@ -0,0 +1,22 @@
+package codingNinja.ArrayQues;
+
+public class sumof_two_Arrays {
+
+	public static void main(String[] args) {
+		int[] arr1 = { 6, 2, 4 };
+		int[] arr2 = { 7, 5, 6 };
+		int[] ans = sumOfTwoArrays(arr1, arr2);
+		display(ans);
+	}
+
+	public static void display(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+	public static int[] sumOfTwoArrays(int[] arr1, int[] arr2) {
+
+	}
+
+}
